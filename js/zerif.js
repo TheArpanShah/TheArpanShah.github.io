952601ab2696b77fd3fdebefdc9cb73bd7ae679f
Arpan Shah <arpan@printecosoftware.com>
2014-11-08 17:12:16 -0600

    other stuff

--- js/zerif.js
+++ js/zerif.js
@@ -1,15 +1,16 @@
 /* ================================
 ===  BACKGROUND SLIDER        ====
 ================================= */ 
+alert("hello");
 
-// $.vegas('slideshow', {
-//   delay:7000,
-//   backgrounds:[
-//     { src:'images/backgrounds/bg1.jpg', fade:1000 },
-//     { src:'images/backgrounds/bg2.jpg', fade:1000 },
-//     { src:'images/backgrounds/bg3.jpg', fade:1000 }
-//   ]
-// });
+$.vegas('slideshow', {
+  delay:7000,
+  backgrounds:[
+    { src:'images/backgrounds/bg1.jpg', fade:1000 },
+    { src:'images/backgrounds/bg2.jpg', fade:1000 },
+    { src:'images/backgrounds/bg3.jpg', fade:1000 }
+  ]
+});
       
 
 /* =================================
